88bcbc34677d3e9924f035cffbf0ce1dbd62af66
Md Zikrullah <mohmmedzikrullah159@gmail.com>
2022-03-10 21:26:15 +0530

    adding controller

---
+++ dotnetapp/WebApp/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.web.MVC;
+
+
+namespace WebApp.Controllers
+{
+    public class UserController : Controllers
+    {
+        public bool isUserPresent(LoginModel data)
+        {
+            return data != null && data.IsUserPresent;
+        }
+    }
+}
