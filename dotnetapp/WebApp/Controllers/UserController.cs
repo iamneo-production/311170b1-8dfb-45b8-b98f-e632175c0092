cd86ed13340979b47b6760b9572fa0b3118465f8
Md Zikrullah <mohmmedzikrullah159@gmail.com>
2022-04-09 17:59:28 +0530

    adding dataAccess

---
+++ dotnetapp/WebApp/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess;
+using System.Net;
+using System;
+using System.Data.Entity.Validation;
+
+namespace WebApp.Controllers
+{
+    public class UserController : ApiController
+    {
+        private static BikeLoanDBEntities entities = new BikeLoanDBEntities();
+        [Route("user/signup")]
+        [HttpPost]
+        public HttpResponseMessage signup([FromBody] User user)
+        {
+            try
+            {
+                entities.Users.Add(user);
+                entities.SaveChanges();
+                var message = Request.CreateResponse(HttpStatusCode.OK, "user added");
+                message.Headers.Location = new Uri(Request.RequestUri + " " + user.email);
+                return message;
+            }
+            catch (DbEntityValidationException ex)
+            {
+                Console.WriteLine(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+        }
+    }
+}
