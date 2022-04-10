8f21acf7925e8b4b454322ba9e4545d9b058b015
v4rsh1th <v4rsh1th@gmail.com>
2022-04-10 15:03:29 +0530

    Admin's dashboard.

--- reactapp/src/components/admin/Login.jsx
+++ reactapp/src/components/admin/Login.jsx
@@ -1,43 +1,43 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import {
-Link, useNavigate
+  Link, useNavigate
 } from "react-router-dom";
 
 export default function Login() {
-const navigate = useNavigate();
-const [email, setEmail] = useState("");
-const [pass, setPassword] = useState("");
-const login = async (e) => {
-  e.preventDefault();
-  const item = {
-    "email": email,
-    "password": pass
-  }
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [pass, setPassword] = useState("");
+  const login = async (e) => {
+    e.preventDefault();
+    const item = {
+      "email": email,
+      "password": pass
+    };
 
-  try {
+    try {
       let result = await fetch('http://localhost:54754/admin/login', {
-      method: 'POST',
-      body: JSON.stringify(item),
-      headers: {
-        "Content-Type": 'application/json',
-        "Accept": 'application/json'
+        method: 'POST',
+        body: JSON.stringify(item),
+        headers: {
+          "Content-Type": 'application/json',
+          "Accept": 'application/json'
+        }
+      });
+
+      result = await result.json();
+
+      if (result == "Admin") {
+        alert("Login Success");
+        sessionStorage.setItem("login-info", item.email);
+        sessionStorage.setItem("role", result);
+        navigate("/admin/getAllLoans");
+      } else {
+        alert("Invalid User");
       }
-    })
-
-    result = await result.json();
-
-    if (result == "Admin") {
-      alert("Login Success")
-      sessionStorage.setItem("login-info", item.email);
-      sessionStorage.setItem("role", result);
-      navigate("/admin/getAllLoans");
-    } else {
-      alert("Invalid User");
+    } catch (err) {
+      alert(err);
     }
-  } catch(err){
-    alert(err);
-  }
-}
+  };
   return (
     <div className="wrapper">
       <div className="squeeze_wrapper">
@@ -52,9 +52,9 @@ const login = async (e) => {
             <form onSubmit={login}>
               <h4 id="login_">Admin</h4>
               <label>Email</label>
-              <input type="email" id="email" placeholder="name@domain" required className="email" onChange={e=>setEmail(e.target.value)}/>
+              <input type="email" id="email" placeholder="name@domain" required className="email" onChange={e => setEmail(e.target.value)} />
               <label>Password</label>
-              <input type="password" id="password" placeholder="Password" required onChange={e=>setPassword(e.target.value)} /><br /><br />
+              <input type="password" id="password" placeholder="Password" required onChange={e => setPassword(e.target.value)} /><br /><br />
               <button type="submit" id="admin_button">Login</button>
             </form>
           </div>
@@ -62,7 +62,7 @@ const login = async (e) => {
           <div className="not_a">
             <p>
               Didn't register as admin yet? &nbsp;
-              <Link to="/admin/signup">Signup</Link>
+              <Link to="/admin/signup">Admin Signup</Link>
             </p>
             <p>
               Not Admin? &nbsp;
